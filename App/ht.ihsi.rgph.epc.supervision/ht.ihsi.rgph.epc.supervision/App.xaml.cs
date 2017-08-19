b0a288c087499bd2a1ebe179d3a5cab9f1923672
Jeff ADME <JAdme@ihsi2010.local>
2017-08-19 09:26:33 -0700

    Creating project

---
+++ App/ht.ihsi.rgph.epc.supervision/ht.ihsi.rgph.epc.supervision/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ht.ihsi.rgph.epc.supervision
+{
+    /// <summary>
+    /// Logique d'interaction pour App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
